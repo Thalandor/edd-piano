a31266077726ca5a7518084ec5225275da8d6348
IvanBV <thalandor1985@hotmail.com>
2021-04-11 23:41:42 +0200

    Add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import './App.css';
+import React from 'react';
+import { Route, BrowserRouter as Router, Switch, NavLink } from 'react-router-dom';
+import './App.scss';
+import Market from './views/Market';
 import PianoView from './views/Piano';
 
 function App() {
   return (
-    <div className="App">
-     <PianoView></PianoView>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="header">
+          <NavLink to="/" >Piano</NavLink>
+          <NavLink to="/market" >Market</NavLink>
+        </header>
+        <Switch>
+          <Route exact path="/">
+            <PianoView></PianoView>
+          </Route>
+          <Route path="/market">
+            <Market />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+
   );
 }
 
